64122636518d6b716186fb10a8bb3ee973a9c959
mayconxrz@gmail.com <mayconxrz@gmail.com>
2023-09-04 18:38:57 -0400

    new commit

--- src/templates/Dashboard/index.tsx
+++ src/templates/Dashboard/index.tsx
@@ -1,16 +1,24 @@
 import { MovieCategory } from '@/components/MovieCategory';
-import { useDashboard } from '@/hooks';
+import { getData } from '@/services/api';
 
 import * as S from './styles';
 
-export const Dashboard = () => {
-  const { categories } = useDashboard();
+export async function getStaticProps() {
+  const { genres } = await getData('/genre/movie/list', { language: 'pt' });
+
+  return {
+    props: { genres }
+  };
+}
+
+export const Dashboard = ({ genres }: any) => {
+  if(!genres) return;
 
   return (
     <>
       <S.Container>
-        {categories.map((category: { id: string; name: string }) => (
-          <MovieCategory key={category.id} category={category} />
+        {genres.map((genre: { id: string; name: string }) => (
+          <MovieCategory key={genre.id} category={genre} />
         ))}
       </S.Container>
     </>
